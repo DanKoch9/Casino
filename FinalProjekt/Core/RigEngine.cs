6e6ddb5c027715858601a5c42ab7b7bddc0258d4
Dan <dany.koch.9@gmail.com>
2026-03-26 15:09:45 +0100

    fixnut rigengine

--- FinalProjekt/Core/RigEngine.cs
+++ FinalProjekt/Core/RigEngine.cs
@@ -2,7 +2,6 @@ namespace FinalProjekt.Core;
 
 public class RigEngine
 {
-    private const string DepositedPath = "../../../Data/deposited.txt";
     private int _consecutiveLosses = 0;
 
     //0.01 je extreme malo, 0.1 je malo, 0.5 je pohoda, 0.9 skoro zarucenej win.
@@ -13,17 +12,16 @@ public class RigEngine
         if (won) _consecutiveLosses = 0;
         else _consecutiveLosses++;
     }
-
     public bool IsWinAllowed(Account account)
     {
         double luckRoll = Random.Shared.NextDouble();
 
-        double balanceRatio = account.Balance / Math.Max(1.0, account.Deposited);
-        
+        double balanceRatio = account.Balance / Math.Clamp(account.Deposited, 1.0, 100000);
+
         double winChance = _winProbability / Math.Max(0.1, balanceRatio);
         
-        winChance += (_consecutiveLosses * 0.01);
-        
+        winChance += (_consecutiveLosses * 0.02);
+
         winChance = Math.Clamp(winChance, 0.005, _winProbability * 2);
 
         return luckRoll <= winChance;
